9eaef702673671b36b34987a4e20fcf7d8185513
bleriot14 <aybarto@gmail.com>
2023-01-21 17:02:15 +0300

    initial

---
+++ ft_userdata/user_data/strategies/dema.py
@@ -0,0 +1,92 @@
+# pragma pylint: disable=missing-docstring, invalid-name, pointless-string-statement
+# flake8: noqa: F401
+# isort: skip_file
+# --- Do not remove these libs ---
+from functools import reduce
+import numpy as np  # noqa
+import pandas as pd  # noqa
+from pandas import DataFrame
+from datetime import datetime
+from typing import Optional
+from freqtrade.strategy import (
+    BooleanParameter,
+    CategoricalParameter,
+    DecimalParameter,
+    IStrategy,
+    IntParameter,
+)
+
+# --------------------------------
+# Add your lib to import here
+import talib.abstract as ta 
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+
+# This class is a sample. Feel free to customize it.
+class dema(IStrategy):
+
+    INTERFACE_VERSION = 3
+
+    # Can this strategy go short?
+    can_short: True
+    minimal_roi = {"0": 0.35}
+    stoploss = -0.15
+
+    def leverage(self, pair: str, current_time: datetime, current_rate: float,
+                 proposed_leverage: float, max_leverage: float, entry_tag: Optional[str], side: str,
+                 **kwargs) -> float:
+
+        return 40
+
+    # Trailing stoploss
+    trailing_stop = False
+    timeframe = '5m'
+
+    # Run "populate_indicators()" only for new candle.
+    process_only_new_candles = False
+    use_exit_signal = True
+    exit_profit_only = False
+    ignore_roi_if_entry_signal = False
+
+
+    startup_candle_count: int = 10
+
+    # Optional order type mapping.
+
+    # Optional order time in force.
+    order_time_in_force = {
+        'entry': 'GTC',
+        'exit': 'GTC'
+    }
+    def informative_pairs(self):
+        """
+        """
+        return []
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        dataframe['dema'] = ta.DEMA(dataframe, timeperiod=9)
+        return dataframe
+
+    def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Based on TA indicators, populates the entry signal for the given dataframe
+        :param dataframe: DataFrame
+        :param metadata: Additional information, like the currently traded pair
+        :return: DataFrame with entry columns populated
+        """
+        dataframe.loc[
+            (
+                (dataframe['close'] <= dataframe['dema']) &  
+                (dataframe['volume'] > 0)  # Make sure Volume is not 0
+            ),
+            'enter_long'] = 1
+
+        dataframe.loc[
+            (
+
+                (dataframe['close'] > dataframe['dema']) & 
+                (dataframe['volume'] > 0)  # Make sure Volume is not 0
+            ),
+            'enter_short'] = 1
+        return dataframe
+    def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        return dataframe
