eeecd75843313b46456edf31fec9b6dc20fe8690
bleriot14 <83162265+bleriot14@users.noreply.github.com>
2023-01-26 22:30:06 +0300

    dddd

--- ft_userdata/user_data/strategies/dema.py
+++ ft_userdata/user_data/strategies/dema.py
@@ -1,4 +1,4 @@
-# pragma pylint: disable=missing-docstring, invalid-name, pointless-string-statement
+# pragma pylint: disable=missing-docstring, invalid-name, pointless-string-statement 
 # flake8: noqa: F401
 # isort: skip_file
 # --- Do not remove these libs ---
